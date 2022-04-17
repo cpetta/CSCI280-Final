ce5387209aac2499fda6fa99648dc56582abea77
Chris Petta <cpetta@oaisd.org>
2022-04-17 15:47:10 -0400

    Add comments and whitespace

--- project/Chess/AI.cs
+++ project/Chess/AI.cs
@@ -23,25 +23,20 @@ namespace Chess
                 t.Fitness = t.Board.fitness(player);
                 return t;
             }
-            
-            depth--;
 
             if (player == MAX)
             {
                 t.Fitness = int.MinValue;
                 foreach (Tree child in t.children)
-                {
-                    t.Fitness = Math.Max(t.Fitness, MiniMax(child, MIN, depth).Fitness);
-                }
+                    t.Fitness = Math.Max(t.Fitness, MiniMax(child, MIN, depth - 1).Fitness);
             }
             else
             {
                 t.Fitness = int.MaxValue;
                 foreach (Tree child in t.children)
-                {
-                    t.Fitness = Math.Min(t.Fitness, MiniMax(child, MAX, depth).Fitness);
-                }
+                    t.Fitness = Math.Min(t.Fitness, MiniMax(child, MAX, depth - 1).Fitness);
             }
+
             return t;
         }
         public static Tree AlphaBetaPruning(Tree t, Player player, int depth, int a = int.MinValue, int b = int.MaxValue)
@@ -53,14 +48,12 @@ namespace Chess
                 return t;
             }
 
-            depth--;
-
             if (player == MAX)
             {
                 t.Fitness = int.MinValue;
                 foreach (Tree child in t.children)
                 {
-                    t.Fitness = Math.Max(t.Fitness, AlphaBetaPruning(child, MIN, depth, a, b).Fitness);
+                    t.Fitness = Math.Max(t.Fitness, AlphaBetaPruning(child, MIN, depth - 1, a, b).Fitness);
                     if (t.Fitness > b)
                         break;
                     a = Math.Max(a, t.Fitness);
@@ -71,7 +64,7 @@ namespace Chess
                 t.Fitness = int.MaxValue;
                 foreach (Tree child in t.children)
                 {
-                    t.Fitness = Math.Min(t.Fitness, AlphaBetaPruning(child, MAX, depth, a, b).Fitness);
+                    t.Fitness = Math.Min(t.Fitness, AlphaBetaPruning(child, MAX, depth - 1, a, b).Fitness);
                     if (t.Fitness < a)
                         break;
                     b = Math.Min(b, t.Fitness);
@@ -114,6 +107,7 @@ namespace Chess
             Player otherPlayer = player == Player.BLACK ? Player.WHITE : Player.BLACK;
             List<Tree> chessBoards = new List<Tree>();
             List<move_t> moves = LegalMoveSet.GetMoves(board.Board, player);
+
             foreach (move_t move in moves)
             {
                 ChessBoard newBoard = LegalMoveSet.move(board.Board, move);
@@ -121,6 +115,7 @@ namespace Chess
                 newTree.move = move;
                 chessBoards.Add(newTree);
             }
+
             return chessBoards;
         }
 
@@ -128,16 +123,20 @@ namespace Chess
         {
             move_t nullmove = new move_t(new position_t(-1, -1), new position_t(-1, -1));
             move_t bestMove = nullmove;
+
             foreach (Tree child in moveTree)
             {
                 if (!PreviousMoves.Contains(child.move))
                     if (moveTree.Fitness <= child.Fitness)
                         bestMove = child.move;
             }
+
+            // Attempt to prevent the AI from getting stuck in an infinate loop.
             if (PreviousMoves.Count > 10)
                 PreviousMoves.Dequeue();
             PreviousMoves.Enqueue(bestMove);
 
+            // Attempt to prevent the AI from not moving under some circumstances
             if (bestMove.Equals(nullmove))
             {
                 Random rnd = new Random();
